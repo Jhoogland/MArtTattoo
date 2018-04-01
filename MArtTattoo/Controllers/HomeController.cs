cd42bcd35bcdf36c630a1c5bc8eaaa24c6fe2db5
Jeffrey <J.hoogland92@live.nl>
2018-04-01 22:31:20 +0200

    first commit

--- MArtTattoo/Controllers/HomeController.cs
+++ MArtTattoo/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using MArtTattoo.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MArtTattoo.Controllers
 {
@@ -27,11 +21,6 @@ namespace MArtTattoo.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
